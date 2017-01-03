bed2b06738eedf2f448b1ff83b5a86e71c38e32c
Ousmane Y. Abdou <Yuso@NS-YUSO-MacBook-Pro.local>
2017-01-03 12:37:16 -0500

    Hook up the @IBOutlets and @IBActions

--- PokedexApp/Pokemon.swift
+++ PokedexApp/Pokemon.swift
@@ -12,6 +12,13 @@ class Pokemon {
     
     private var _name: String!
     private var _pokemonID: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _defense: String!
+    private var _pokedexID: String!
+    private var _attack: String!
     
     var name: String {
         
--- PokedexApp/PokemonDetailVC.swift
+++ PokedexApp/PokemonDetailVC.swift
@@ -16,9 +16,24 @@ class PokemonDetailVC: UIViewController {
     // Outlets
     
     @IBOutlet weak var nameLBL: UILabel!
+    @IBOutlet weak var mainIMG: UIImageView!
+    @IBOutlet weak var descriptionLBL: UILabel!
+    @IBOutlet weak var typeLBL: UILabel!
+    @IBOutlet weak var heightLBL: UILabel!
+    @IBOutlet weak var weightLBL: UILabel!
+    @IBOutlet weak var defenseLBL: UILabel!
+    @IBOutlet weak var pokedexIDLBL: UILabel!
+    @IBOutlet weak var attackLBL: UILabel!
+    @IBOutlet weak var evolutionLBL: UILabel!
+    @IBOutlet weak var currentEvolutionLBL: UIImageView!
+    @IBOutlet weak var nextEvolutionLBL: UIImageView!
+    
     // Actions
     
-    
+    @IBAction func backBTNPressed(sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
